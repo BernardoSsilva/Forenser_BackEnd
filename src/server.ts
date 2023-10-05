4b7031c17cdbf3757ba40774dc92f00be5082195
BernardoSsilva <Bernardo.ssilva0808@gmail.com>
2023-10-05 05:20:46 -0300

    created database ligation, to do: login and register system

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,28 @@
-import express from "express"
+import express from "express";
 
-const app = express()
+const app = express();
+const mysql = require("mysql");
+const db = mysql.createPool({
+        host:"localhost",
+        user:"root",
+        password: "",
+        database: "forencer_data",
+        })
 
 
 app.get('/', (req,res)=>{
+    db.query("Insert into usuario(nome_usu, cpf_usu, data_nasc, email_usu, telefone, sexo, senha) values ('Bernardo Santos da silva','123.008.389-82','2005-08-08','Bernardosilva698@gmail.com','(48)99852-9084', 'M','bernardo1227')"),(err, result) => {
+        if(err){
+            console.log(err);
+        }
+    };
 
-    res.send('.../forencerApp/src/pages/home.tsx ').end()
 
 })
 
-app.listen(3000, ()=>{
+app.listen(3001, ()=>{
 
-    console.log('listen on port 3000');
+    console.log('listen on port 3001');
     
 
 })
