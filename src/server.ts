d3e8f191c79356bd62df58c652abfa69dd7e3e27
BernardoSsilva <Bernardo.ssilva0808@gmail.com>
2023-10-05 08:39:49 -0300

    socorro elegê

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 
+const cors = require('cors'); 
 const app = express();
 const mysql = require("mysql");
 const db = mysql.createPool({
@@ -7,15 +8,40 @@ const db = mysql.createPool({
         user:"root",
         password: "",
         database: "forencer_data",
-        })
+        });
+
+
+app.use(express.json());
+app.use(cors());
+
+app.post("https://localhost:3001/register", (req,res) => {
+    const nome = req.body.nome;
+    const email= req.body.email;
+    const password = req.body.password;
+    const telefone = req.body.telefone;
+    const sexo = req.body.sexo;
+    const cpf = req.body.cpf;
+    const data_n = req.body.data_n;
+
+    db.query("Select * from usuario where email_usu = ?", [email], (err:any, result:any) => {
+        if(err){
+            res.send(err);
+        } if(result.lenght == 0){
+            db.query("Insert into usuario(nome_usu, cpf_usu, data_nasc, email_usu, telefone, sexo, senha) values (?,?,?,?,?,?,?)",{nome, cpf, data_n, email, telefone, sexo, password})
+        }
+        res.send(result)
+    })
+})
+
 
 
 app.get('/', (req,res)=>{
-    db.query("Insert into usuario(nome_usu, cpf_usu, data_nasc, email_usu, telefone, sexo, senha) values ('Bernardo Santos da silva','123.008.389-82','2005-08-08','Bernardosilva698@gmail.com','(48)99852-9084', 'M','bernardo1227')"),(err, result) => {
-        if(err){
-            console.log(err);
-        }
-    };
+    //db.query("Insert into usuario(nome_usu, cpf_usu, data_nasc, email_usu, telefone, sexo, senha) values ('Bernardo Santos da silva','123.008.389-82','2005-08-08','Bernardosilva698@gmail.com','(48)99852-9084', 'M','bernardo123456789')"),(err, result) => {
+    //    if(err){
+    //        console.log(err);
+    //    }
+    console.log("oi");
+    //};
 
 
 })
