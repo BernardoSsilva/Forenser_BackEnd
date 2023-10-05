8f5ba399bfdc9899c0782fad6ba5a7972ab64156
BernardoSsilva <Bernardo.ssilva0808@gmail.com>
2023-10-05 10:34:22 -0300

    socorro lgbt 2

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ const db = mysql.createPool({
 app.use(express.json());
 app.use(cors());
 
-app.post("https://localhost:3001/register", (req,res) => {
+app.post("/register", (req,res) => {
     const nome = req.body.nome;
     const email= req.body.email;
     const password = req.body.password;
@@ -23,28 +23,25 @@ app.post("https://localhost:3001/register", (req,res) => {
     const cpf = req.body.cpf;
     const data_n = req.body.data_n;
 
-    db.query("Select * from usuario where email_usu = ?", [email], (err:any, result:any) => {
-        if(err){
-            res.send(err);
-        } if(result.lenght == 0){
-            db.query("Insert into usuario(nome_usu, cpf_usu, data_nasc, email_usu, telefone, sexo, senha) values (?,?,?,?,?,?,?)",{nome, cpf, data_n, email, telefone, sexo, password})
-        }
-        res.send(result)
-    })
+    //db.query("Select * from usuario where email_usu = ?", [email], (err:any, result:any) => {
+      //  if(err){
+        //    res.send(err);
+        //} if(result.lenght == 0){
+            db.query("Insert into usuario(nome_usu, cpf_usu, data_nasc, email_usu, telefone, sexo, senha) values (?,?,?,?,?,?,?)",
+            [nome, cpf, data_n, email, telefone, sexo, password], (err:any, result:any) => {
+                if(err){
+                    result.send(err)
+                } else{
+                    result.send({msg: "Usuario cadastrado com sucesso"})
+                }
+            });
+        //} else{
+          //  res.send({msg: "usuario ja cadastrado"})
+        //}
+        //res.send({msg: "usuario ja cadastrado"})
+    //})
 })
 
-
-
-app.get('/', (req,res)=>{
-    //db.query("Insert into usuario(nome_usu, cpf_usu, data_nasc, email_usu, telefone, sexo, senha) values ('Bernardo Santos da silva','123.008.389-82','2005-08-08','Bernardosilva698@gmail.com','(48)99852-9084', 'M','bernardo123456789')"),(err, result) => {
-    //    if(err){
-    //        console.log(err);
-    //    }
-    console.log("oi");
-    //};
-
-
-})
 
 app.listen(3001, ()=>{
 
