b5bd03ae46d2b622fb3a43dd38fd4ae5815270b1
BernardoSsilva <Bernardo.ssilva0808@gmail.com>
2023-10-24 16:23:48 -0300

    realizando cadastro no banco para todos os tipos de boletim de ocorrencia

--- src/server.ts
+++ src/server.ts
@@ -221,6 +221,44 @@ app.post("/registrarRoubo", (req,res) =>{
   }
 })
 
+app.post("/registrarViolenciaD", (req,res) =>{
+  const violencia = req.body.violencia;
+  const obito = req.body.obito;
+  const data_fato = dateFormatter.format(new Date(req.body.data_fato));
+  const horario = req.body.horario;
+  const tipo_local = req.body.tipo_local;
+  const endereco = req.body.endereco;
+  const comunicante = req.body.comunicante;
+  const vitima = req.body.vitima;
+  const objetos = req.body.objetos;
+  const relato_fato = req.body.relato_fato;
+  try{
+    connection.query(
+      "INSERT INTO boletim_violenciaDomestica SET ?",
+      {
+        violencia,
+        obito,
+        data_fato,
+        horario,
+        tipo_local,
+        endereco,
+        comunicante,
+        vitima,
+        relato_fato
+      },
+      
+      function (error, results, fields) {
+        console.log(error, results, fields);
+
+        if (error) throw error;
+        console.log(results.insertId);
+      }
+      )
+  }catch(error){
+    console.log(error)
+  }
+})
+
 
 
 app.get('/inicio', verifyJwt, (req, res) =>{
