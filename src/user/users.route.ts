e952f1559b0f68fe3217c1fb0230dca86a1e15e0
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-02-14 20:55:04 -0300

    created routes docs

---
+++ src/user/users.route.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import router from "../routes/router";
+
+const userRoute = Router()
+
+userRoute.get("/", (req, res) =>{
+    
+})
+
+userRoute.get("/:id", (req, res) =>{
+
+})
+
+userRoute.post("/register", (req, res) =>{})
+
+userRoute.patch("/edit/:id", (req, res) =>{
+
+})
+
+userRoute.delete("/delete/:id", (req, res) =>{})
