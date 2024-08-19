a66b1e2f340a3fa2889b0101afe33567c5b41db6
BernardoSilva <bernardo.ssilva0808@gmail.com>
2024-08-19 17:58:53 -0300

    :rocket: pull Updates

--- Code/src/ForenserBackend.Domain/obj/Debug/net8.0/ForenserBackend.Domain.AssemblyInfo.cs
+++ Code/src/ForenserBackend.Domain/obj/Debug/net8.0/ForenserBackend.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ForenserBackend.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c9a4a0dab2ed8cad4756e47f0d9b0949e55aece")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+833182f28000f83edd0cdd180e7561e598a54dd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ForenserBackend.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ForenserBackend.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
