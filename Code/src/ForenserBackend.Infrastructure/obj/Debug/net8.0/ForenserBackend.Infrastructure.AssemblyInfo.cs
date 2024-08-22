6a60edbd9f965630a6a3f35a0ce86c50837e9b14
BernardoSilva <bernardo.ssilva0808@gmail.com>
2024-08-22 17:02:33 -0300

    Editor save

--- Code/src/ForenserBackend.Infrastructure/obj/Debug/net8.0/ForenserBackend.Infrastructure.AssemblyInfo.cs
+++ Code/src/ForenserBackend.Infrastructure/obj/Debug/net8.0/ForenserBackend.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ForenserBackend.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e71b43add6f93bfa4ba8d8f9d23159c6c09df73a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+581b2fb74bc66a62c34c976b6773189767e6e28e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ForenserBackend.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ForenserBackend.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
